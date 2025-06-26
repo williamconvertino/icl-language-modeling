4eba2a6ed3aa1cafad9fb8d8b9afad6e5427ceea
William Convertino <williamconvertino@gmail.com>
2025-06-25 21:45:27 -0400

    Added experiments

---
+++ scripts/experiments.sh
@@ -0,0 +1,5 @@
+# Transformer
+
+sbatch --job-name=transformer_8l ./run_job.sh model.n_layers=8
+sbatch --job-name=transformer_6l ./run_job.sh model.n_layers=6
+sbatch --job-name=transformer_4l ./run_job.sh model.n_layers=4
