12400c9a8a1d6464b470435786c81bc02d6375f5
William Convertino <williamconvertino@gmail.com>
2025-06-25 22:01:13 -0400

    Added goodwiki dataset

--- data/goodwiki.py
+++ data/goodwiki.py
@@ -3,24 +3,24 @@ from datasets import load_dataset, DatasetDict
 from icl_lm.data import Tokenizer
 from icl_lm.data import DiskDataset
 
-DATASET_PATH = os.path.join(os.path.dirname(__file__), "datasets", "tinystories_v2")
+DATASET_PATH = os.path.join(os.path.dirname(__file__), "..", "datasets", "goodwiki")
 DATASET_PATH = os.path.abspath(DATASET_PATH)
 
 def initialize_dataset(tokenizer, num_workers):
     
-    train_data = load_dataset("roneneldan/TinyStories", data_files="TinyStoriesV2-GPT4-train.txt", split="train")
-    valid_data = load_dataset("roneneldan/TinyStories", data_files="TinyStoriesV2-GPT4-valid.txt", split="train")
-
-    val_data = valid_data.select(range(10_000))
-    test_data = valid_data.select(range(10_000, len(valid_data)))
-
+    train_data = load_dataset("euirim/goodwiki", split="train")
+    
+    val_data = train_data.select(range(2_000)) # 2k val
+    test_data = train_data.select(range(2_000, 4_000)) # 2k test
+    train_data = train_data.select(range(4_000, len(train_data))) # 40k train
+    
     dataset_dict = DatasetDict({
         "train": train_data,
         "val": val_data,
         "test": test_data
     })
 
-    DiskDataset.build_from_dataset(dataset_dict, tokenizer, "datasets/tinystories_v2", num_workers=num_workers)
+    DiskDataset.build_from_dataset(dataset_dict, tokenizer, "datasets/goodwiki", column="markdown", num_workers=num_workers)
     
 def get_splits(tokenizer, max_seq_len, num_workers=16):
     
@@ -29,7 +29,7 @@ def get_splits(tokenizer, max_seq_len, num_workers=16):
         initialize_dataset(tokenizer, num_workers)
         
     return DiskDataset.get_splits(
-        base_path="datasets/tinystories_v2",
+        base_path="datasets/goodwiki",
         tokenizer=tokenizer,
         max_seq_len=max_seq_len
     )
