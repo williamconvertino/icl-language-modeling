de17c2e4fc14f439df476b02e2d8de06ff559a3f
William Convertino <williamconvertino@gmail.com>
2025-06-29 00:09:27 -0400

    Updated experiments

--- scripts/experiments.sh
+++ scripts/experiments.sh
@@ -4,17 +4,17 @@ sbatch --job-name=transformer_8l ./run_job.sh model.n_layers=8
 sbatch --job-name=transformer_6l ./run_job.sh model.n_layers=6
 sbatch --job-name=transformer_4l ./run_job.sh model.n_layers=4
 
-sbatch --job-name=transformer_8l_goodwiki ./run_job.sh model.n_layers=8 dataset=goodwiki
+# sbatch --job-name=transformer_8l_goodwiki ./run_job.sh model.n_layers=8 dataset=goodwiki
 # sbatch --job-name=transformer_6l_goodwiki ./run_job.sh model.n_layers=6 dataset=goodwiki
 # sbatch --job-name=transformer_4l_goodwiki ./run_job.sh model.n_layers=4 dataset=goodwiki
 
 # ICL
 
 sbatch --job-name=icl_8l ./run_job.sh model=icl model.n_layers=8
-sbatch --job-name=icl_6l ./run_job.sh model=icl model.n_layers=6
-sbatch --job-name=icl_4l ./run_job.sh model=icl model.n_layers=4
+# sbatch --job-name=icl_6l ./run_job.sh model=icl model.n_layers=6
+# sbatch --job-name=icl_4l ./run_job.sh model=icl model.n_layers=4
 
-sbatch --job-name=icl_8l_goodwiki ./run_job.sh model=icl model.n_layers=8 dataset=goodwiki
+# sbatch --job-name=icl_8l_goodwiki ./run_job.sh model=icl model.n_layers=8 dataset=goodwiki
 # sbatch --job-name=icl_6l_goodwiki ./run_job.sh model=icl model.n_layers=6 dataset=goodwiki
 # sbatch --job-name=icl_4l_goodwiki ./run_job.sh model=icl model.n_layers=4 dataset=goodwiki
 
@@ -22,7 +22,7 @@ sbatch --job-name=icl_8l_goodwiki ./run_job.sh model=icl model.n_layers=8 datase
 
 sbatch --job-name=icl_use_wv ./run_job.sh model=icl model.icl_use_wv=true
 # sbatch --job-name=icl_use_ln_mlp ./run_job.sh model=icl model.icl_use_ln_mlp=true
-sbatch --job-name=icl_use_skip_mlp ./run_job.sh model=icl model.icl_use_skip_mlp=true
+# sbatch --job-name=icl_use_skip_mlp ./run_job.sh model=icl model.icl_use_skip_mlp=true
 # sbatch --job-name=icl_use_ln_v ./run_job.sh model=icl model.icl_use_ln_v=true
 # sbatch --job-name=icl_use_ln_qk ./run_job.sh model=icl model.icl_use_ln_qk=true
 
