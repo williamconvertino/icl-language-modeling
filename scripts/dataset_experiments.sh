a43e95abd507454128e388d22f174523df8edd35
William Convertino <wac20@dcc-biostat-gpu-13.rc.duke.edu>
2025-07-02 15:51:54 -0400

    Updated scripts

--- scripts/dataset_experiments.sh
+++ scripts/dataset_experiments.sh
@@ -1,29 +1,29 @@
 # TinyStories
 
-sbatch --job-name=transformer_8l ./run_job.sh model.n_layers=8
-sbatch --job-name=transformer_6l ./run_job.sh model.n_layers=6
-sbatch --job-name=transformer_4l ./run_job.sh model.n_layers=4
+sbatch --job-name=transformer_8l ./lr_search.sh model.n_layers=8
+sbatch --job-name=transformer_6l ./lr_search.sh model.n_layers=6
+sbatch --job-name=transformer_4l ./lr_search.sh model.n_layers=4
 
-sbatch --job-name=icl ./run_job.sh model=icl
-sbatch --job-name=icl_6l ./run_job.sh model=icl model.n_layers=6
-sbatch --job-name=icl_4l ./run_job.sh model=icl model.n_layers=4
+sbatch --job-name=icl ./lr_search.sh model=icl
+# sbatch --job-name=icl_6l ./lr_search.sh model=icl model.n_layers=6
+# sbatch --job-name=icl_4l ./lr_search.sh model=icl model.n_layers=4
 
 # GoodWiki
 
-sbatch --job-name=transformer_8l_goodwiki ./run_job.sh model.n_layers=8 dataset=goodwiki
-sbatch --job-name=transformer_6l_goodwiki ./run_job.sh model.n_layers=6 dataset=goodwiki
-sbatch --job-name=transformer_4l_goodwiki ./run_job.sh model.n_layers=4 dataset=goodwiki
+sbatch --job-name=transformer_8l_goodwiki ./lr_search.sh model.n_layers=8 dataset=goodwiki
+sbatch --job-name=transformer_6l_goodwiki ./lr_search.sh model.n_layers=6 dataset=goodwiki
+sbatch --job-name=transformer_4l_goodwiki ./lr_search.sh model.n_layers=4 dataset=goodwiki
 
-sbatch --job-name=icl_8l_goodwiki ./run_job.sh model=icl model.n_layers=8 dataset=goodwiki
-sbatch --job-name=icl_6l_goodwiki ./run_job.sh model=icl model.n_layers=6 dataset=goodwiki
-sbatch --job-name=icl_4l_goodwiki ./run_job.sh model=icl model.n_layers=4 dataset=goodwiki
+sbatch --job-name=icl_8l_goodwiki ./lr_search.sh model=icl model.n_layers=8 dataset=goodwiki
+# sbatch --job-name=icl_6l_goodwiki ./lr_search.sh model=icl model.n_layers=6 dataset=goodwiki
+# sbatch --job-name=icl_4l_goodwiki ./lr_search.sh model=icl model.n_layers=4 dataset=goodwiki
 
 # TinyStories V2
 
-sbatch --job-name=transformer_8l_v2 ./run_job.sh model.n_layers=8 dataset=tinystories_v2
-sbatch --job-name=transformer_6l_v2 ./run_job.sh model.n_layers=6 dataset=tinystories_v2
-sbatch --job-name=transformer_4l_v2 ./run_job.sh model.n_layers=4 dataset=tinystories_v2
+sbatch --job-name=transformer_8l_v2 ./lr_search.sh model.n_layers=8 dataset=tinystories_v2
+sbatch --job-name=transformer_6l_v2 ./lr_search.sh model.n_layers=6 dataset=tinystories_v2
+sbatch --job-name=transformer_4l_v2 ./lr_search.sh model.n_layers=4 dataset=tinystories_v2
 
-sbatch --job-name=icl_8l_v2 ./run_job.sh model=icl model.n_layers=8 dataset=tinystories_v2
-sbatch --job-name=icl_6l_v2 ./run_job.sh model=icl model.n_layers=6 dataset=tinystories_v2
-sbatch --job-name=icl_4l_v2 ./run_job.sh model=icl model.n_layers=4 dataset=tinystories_v2
+sbatch --job-name=icl_8l_v2 ./lr_search.sh model=icl model.n_layers=8 dataset=tinystories_v2
+# sbatch --job-name=icl_6l_v2 ./lr_search.sh model=icl model.n_layers=6 dataset=tinystories_v2
+# sbatch --job-name=icl_4l_v2 ./lr_search.sh model=icl model.n_layers=4 dataset=tinystories_v2
--- scripts/modification_experiments.sh
+++ scripts/modification_experiments.sh
@@ -1,22 +1,22 @@
 # Misc Modifications
 
-sbatch --job-name=use_mlp_out ./run_job.sh model=icl model.use_mlp_out=true
-sbatch --job-name=icl_use_skip_mlp ./run_job.sh model=icl model.icl_use_skip_mlp=true
-sbatch --job-name=icl_use_wv ./run_job.sh model=icl model.icl_use_wv=true
-sbatch --job-name=icl_use_ln ./run_job.sh model=icl model.icl_use_ln=true
-sbatch --job-name=icl_use_rotary ./run_job.sh model=icl model.icl_use_rotary_embedding=true
+sbatch --job-name=use_mlp_out ./lr_search.sh model=icl model.use_mlp_out=true
+sbatch --job-name=icl_use_skip_mlp ./lr_search.sh model=icl model.icl_use_skip_mlp=true
+sbatch --job-name=icl_use_wv ./lr_search.sh model=icl model.icl_use_wv=true
+sbatch --job-name=icl_use_ln ./lr_search.sh model=icl model.icl_use_ln=true
+sbatch --job-name=icl_use_rotary ./lr_search.sh model=icl model.icl_use_rotary_embedding=true
 
 # Shared weights
 
-sbatch --job-name=share_covariate_attn ./run_job.sh model=icl model.n_layers=8 model.share_covariate_attn=true
-sbatch --job-name=share_covariate_mlp ./run_job.sh model=icl model.n_layers=8 model.share_covariate_mlp=true
-sbatch --job-name=share_icl_attn ./run_job.sh model=icl model.n_layers=8 model.share_icl_attn=true
-sbatch --job-name=share_icl_mlp ./run_job.sh model=icl model.n_layers=8 model.share_icl_mlp=true
+sbatch --job-name=share_covariate_attn ./lr_search.sh model=icl model.n_layers=8 model.share_covariate_attn=true
+sbatch --job-name=share_covariate_mlp ./lr_search.sh model=icl model.n_layers=8 model.share_covariate_mlp=true
+sbatch --job-name=share_icl_attn ./lr_search.sh model=icl model.n_layers=8 model.share_icl_attn=true
+sbatch --job-name=share_icl_mlp ./lr_search.sh model=icl model.n_layers=8 model.share_icl_mlp=true
 
 # Alternate Order
 
-sbatch --job-name=alt_1 ./run_job.sh model=icl model.block_order=["T", "T", "I", "T", "T", "I", "T", "I"]
-sbatch --job-name=alt_2 ./run_job.sh model=icl model.block_order=["T", "T", "I", "I", "T", "T", "I", "I"]
-sbatch --job-name=alt_3 ./run_job.sh model=icl model.block_order=["T", "T", "T", "T", "T", "T", "I", "I"]
-sbatch --job-name=alt_4 ./run_job.sh model=icl model.block_order=["T", "T", "T", "I", "T", "T", "T", "I"]
-sbatch --job-name=alt_5 ./run_job.sh model=icl model.block_order=["T", "I", "I", "T", "I", "I", "T", "I"]
+sbatch --job-name=alt_1 ./lr_search.sh model=icl model.block_order=["T", "T", "I", "T", "T", "I", "T", "I"]
+sbatch --job-name=alt_2 ./lr_search.sh model=icl model.block_order=["T", "T", "I", "I", "T", "T", "I", "I"]
+sbatch --job-name=alt_3 ./lr_search.sh model=icl model.block_order=["T", "T", "T", "T", "T", "T", "I", "I"]
+sbatch --job-name=alt_4 ./lr_search.sh model=icl model.block_order=["T", "T", "T", "I", "T", "T", "T", "I"]
+sbatch --job-name=alt_5 ./lr_search.sh model=icl model.block_order=["T", "I", "I", "T", "I", "I", "T", "I"]
