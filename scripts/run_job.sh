113df1cf0e2442590ae903a2c35d32d0cfe0c628
William Convertino <williamconvertino@gmail.com>
2025-07-03 14:25:43 -0400

    Fixed bad node

--- scripts/run_job.sh
+++ scripts/run_job.sh
@@ -8,7 +8,7 @@
 #SBATCH --time=48:00:00
 #SBATCH --partition=scavenger-gpu
 #SBATCH --exclude=dcc-youlab-gpu-28,dcc-gehmlab-gpu-56
-#SBATCH --nodelist=dcc-allenlab-gpu-[01-04],dcc-allenlab-gpu-[05-12],dcc-majoroslab-gpu-[01-08],dcc-yaolab-gpu-[01-08],dcc-wengerlab-gpu-01,dcc-engelhardlab-gpu-[01-04],dcc-motesa-gpu-[01-04],dcc-pbenfeylab-gpu-[01-04],dcc-vossenlab-gpu-[01-04],dcc-youlab-gpu-[01-56],dcc-mastatlab-gpu-01,dcc-viplab-gpu-01,dcc-youlab-gpu-57
+#SBATCH --nodelist=dcc-allenlab-gpu-[01-04],dcc-allenlab-gpu-[05-12],dcc-majoroslab-gpu-[01-08],dcc-yaolab-gpu-[01-08],dcc-wengerlab-gpu-01,dcc-engelhardlab-gpu-[02-04],dcc-motesa-gpu-[01-04],dcc-pbenfeylab-gpu-[01-04],dcc-vossenlab-gpu-[01-04],dcc-youlab-gpu-[01-56],dcc-mastatlab-gpu-01,dcc-viplab-gpu-01,dcc-youlab-gpu-57
 #HIGH VRAM --nodelist=dcc-allenlab-gpu-[01-04],dcc-allenlab-gpu-[05-12],dcc-majoroslab-gpu-[01-08]
 #SBATCH --requeue
 #SBATCH --output=logs/%x-%j.out
