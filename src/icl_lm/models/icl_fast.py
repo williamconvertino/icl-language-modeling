0b867a1d1eabc84b9f9c296589dba1874c8109d5
William Convertino <williamconvertino@gmail.com>
2025-07-11 19:35:28 -0400

    Fixed mask shape

--- src/icl_lm/models/icl_fast.py
+++ src/icl_lm/models/icl_fast.py
@@ -3,8 +3,90 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from .lm_base import LMBase
-from .transformer_fast import MLP, TransformerBlock
+from torchtune.modules import RotaryPositionalEmbeddings
 
+class Attention(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        
+        self.config = config
+        
+        self.W_q = nn.Linear(config.hidden_dim, config.hidden_dim, bias=False)
+        self.W_k = nn.Linear(config.hidden_dim, config.hidden_dim, bias=False)
+        self.W_v = nn.Linear(config.hidden_dim, config.hidden_dim, bias=False)
+        self.W_o = nn.Linear(config.hidden_dim, config.hidden_dim, bias=False)
+        
+        self.attn_scale = 1 / math.sqrt(config.hidden_dim)
+        
+        self.rotary_embeddings = RotaryPositionalEmbeddings(config.hidden_dim // config.n_heads, max_seq_len=config.max_seq_len)
+        
+        self.drop_attn = nn.Dropout(0.1)
+        self.drop_resid = nn.Dropout(0.1)
+        
+        causal_mask = torch.triu(torch.ones(config.max_seq_len, config.max_seq_len), diagonal=1).bool()
+        
+        self.register_buffer("causal_mask", causal_mask)
+    
+    def forward(self, x):
+        
+        q = k = v = x
+        
+        B, S, E = q.shape
+        
+        q = self.W_q(q).view(B, S, self.config.n_heads, self.config.hidden_dim // self.config.n_heads).transpose(1, 2)
+        k = self.W_k(k).view(B, S, self.config.n_heads, self.config.hidden_dim // self.config.n_heads).transpose(1, 2)
+        v = self.W_v(v).view(B, S, self.config.n_heads, self.config.hidden_dim // self.config.n_heads).transpose(1, 2)
+        
+        q = self.rotary_embeddings(q)
+        k = self.rotary_embeddings(k)
+
+        attn_scores = torch.matmul(q, k.transpose(-2, -1)) * self.attn_scale
+        attn_scores = attn_scores.masked_fill(self.causal_mask, float('-inf'))
+        
+        attn_probs = F.softmax(attn_scores, dim=-1)
+        attn_probs = self.drop_attn(attn_probs)
+        
+        attn_output = torch.matmul(attn_probs, v)
+        attn_output = attn_output.transpose(1, 2).contiguous().view(B, S, self.config.hidden_dim)
+        attn_output = self.W_o(attn_output)
+        attn_output = self.drop_resid(attn_output)
+        
+        return attn_output
+    
+class MLP(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+
+        self.fc_1 = nn.Linear(config.hidden_dim, 4 * config.hidden_dim, bias=True)
+        self.fc_2 = nn.Linear(4 * config.hidden_dim, config.hidden_dim, bias=True)
+        
+        self.activation = nn.GELU()
+        self.drop = nn.Dropout(0.1)
+
+    def forward(self, x):
+        x = self.fc_1(x)
+        x = self.activation(x)
+        x = self.drop(x)
+        x = self.fc_2(x)
+        return x
+    
+class TransformerBlock(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        
+        self.config = config
+        
+        self.attention = Attention(config)
+        self.ln_attn = nn.LayerNorm(config.hidden_dim)
+        
+        self.mlp = MLP(config)
+        self.ln_mlp = nn.LayerNorm(config.hidden_dim)
+        
+    def forward(self, x):
+        x = x + self.attention(self.ln_attn(x))
+        x = x + self.mlp(self.ln_mlp(x))
+        return x
+    
 class ICLAttention(nn.Module):
     def __init__(self, config):
         super().__init__()
