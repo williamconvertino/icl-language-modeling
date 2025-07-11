58233b050da790c2918a731f3a8d5336681090cc
William Convertino <wac20@dcc-youlab-gpu-57.rc.duke.edu>
2025-07-11 14:06:37 -0400

    Updated permissions

--- scripts/experiments.sh
+++ scripts/experiments.sh
@@ -1,4 +1,4 @@
 sbatch --job-name=transformer_medium ./train_medium.sh model=transformer_medium
-sbatch --job-name=icl_medium ./train_medium.sh model=icl_medium
-sbatch --job-name=icl_medium_mlp ./train_medium.sh model=icl_medium model.icl_use_mlp=true
-sbatch --job-name=icl_medium_output_mlp ./train_medium.sh model=icl_medium model.use_output_mlp=true
+# sbatch --job-name=icl_medium ./train_medium.sh model=icl_medium
+# sbatch --job-name=icl_medium_mlp ./train_medium.sh model=icl_medium model.icl_use_mlp=true
+# sbatch --job-name=icl_medium_output_mlp ./train_medium.sh model=icl_medium model.use_output_mlp=true
