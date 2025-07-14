e7b1f20772c1ea91907e70fe610c887dcd973ea3
William Convertino <williamconvertino@gmail.com>
2025-07-14 13:32:24 -0400

    Updated scripts

--- scripts/kill_jobs.sh
+++ scripts/kill_jobs.sh
@@ -1,2 +1,2 @@
-squeue -u $USER | awk '$0 ~ /trans|icl/ {print $1}' | xargs -r scancel
+squeue -u $USER | awk '$0 ~ /trans|icl|red/ && $0 !~ /h200/ {print $1}' | xargs -r scancel
 squeue -u $USER | awk '$0 ~ /h200/ {print $1}' | xargs -r scancel
